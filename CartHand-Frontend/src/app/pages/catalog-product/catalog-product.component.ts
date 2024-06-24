5a5b61b8cea3cb87098a9581a8e244e2607c296a
unknown <wennedchaves1997@gmail.com>
2024-06-24 16:10:01 -0300

    feat: creating component page catalog products

---
+++ CartHand-Frontend/src/app/pages/catalog-product/catalog-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalog-product.component.html',
+  styleUrl: './catalog-product.component.scss'
+})
+export class CatalogProductComponent {
+
+}
