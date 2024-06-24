12168cf4fecca274186b43411ce6f6e65562b97c
unknown <wennedchaves1997@gmail.com>
2024-06-24 14:33:57 -0300

    feat: creating router page register product

--- CartHand-Frontend/src/app/app.routes.ts
+++ CartHand-Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterProductComponent } from './pages/register-product/register-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: "register", component: RegisterProductComponent },
+
+];
