3bcda067167fe7caf8ca2195f6bb1be0f5a51524
unknown <wennedchaves1997@gmail.com>
2024-06-25 14:51:21 -0300

    feat: import data product page catalog-product

--- CartHand-Frontend/src/app/pages/catalog-product/catalog-product.component.ts
+++ CartHand-Frontend/src/app/pages/catalog-product/catalog-product.component.ts
@@ -3,10 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-catalog-product',
   standalone: true,
-  imports: [],
+  imports: [CatalogProductComponent],
   templateUrl: './catalog-product.component.html',
   styleUrl: './catalog-product.component.scss'
 })
 export class CatalogProductComponent {
+
+  product = {
+
+    name: "Bolacha", 
+    price: 2.85,
+    dataOfValidated: "01/08/2025",
+    unitOfMeasurement: "300g"
+
+  }
 
 }
