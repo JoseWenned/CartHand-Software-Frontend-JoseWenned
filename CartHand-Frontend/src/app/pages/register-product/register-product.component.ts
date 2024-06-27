67db8fc7ae54cb20370ddef2acfe48a9220f0fed
unknown <wennedchaves1997@gmail.com>
2024-06-27 10:03:45 -0300

    feat: import component registerItemProduct in page

--- CartHand-Frontend/src/app/pages/register-product/register-product.component.ts
+++ CartHand-Frontend/src/app/pages/register-product/register-product.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { FormRegisterProductComponent } from "../../components/form-register-product/form-register-product.component";
 
 @Component({
     selector: 'app-register-product',
     standalone: true,
     templateUrl: './register-product.component.html',
     styleUrl: './register-product.component.scss',
-    imports: [FormRegisterProductComponent]
+    imports: [RegisterItemProductComponent]
 })
 export class RegisterProductComponent {
 
