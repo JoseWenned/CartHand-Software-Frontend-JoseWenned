0babf45664ea49dc0912572993d3610dede58bfe
unknown <wennedchaves1997@gmail.com>
2024-07-16 04:54:19 -0300

    feat: creating component card list products

--- CartHand-Frontend/src/app/components/list-itens-products/list-itens-products.component.ts
+++ CartHand-Frontend/src/app/components/list-itens-products/list-itens-products.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardItensProductsComponent } from "../card-itens-products/card-itens-products.component";
 
 @Component({
   selector: 'app-list-itens-products',
   standalone: true,
-  imports: [],
+  imports: [CardItensProductsComponent],
   templateUrl: './list-itens-products.component.html',
   styleUrl: './list-itens-products.component.scss'
 })
