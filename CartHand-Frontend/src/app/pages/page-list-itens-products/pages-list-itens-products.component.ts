37192e884f9b1c31f3a1920e677d8c0ba70f8601
unknown <wennedchaves1997@gmail.com>
2024-07-16 16:14:29 -0300

    feat: creating new page list itens products

---
+++ CartHand-Frontend/src/app/pages/page-list-itens-products/pages-list-itens-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-list-itens-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './pages-list-itens-products.component.html',
+  styleUrl: './pages-list-itens-products.component.scss'
+})
+export class PagesListItensProductsComponent {
+
+}
