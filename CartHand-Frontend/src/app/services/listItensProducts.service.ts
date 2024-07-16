6c2e77893c27961ca3b1275172a16c961f88b18f
unknown <wennedchaves1997@gmail.com>
2024-07-16 16:59:30 -0300

    feat: creating function read itens products and add itens products in list products

--- CartHand-Frontend/src/app/services/listItensProducts.service.ts
+++ CartHand-Frontend/src/app/services/listItensProducts.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from "@angular/core";
+import { IItemProduct, IItemProductCreate } from "../interfaces/itemProduct.interface";
 
 @Injectable({
     providedIn: "root"
 })
 export class listItensProductService {
 
-    private listItensProducts = [];
+    private listItensProducts: IItemProduct[] = [];
+
+    getAllItensProduct(){
+        return this.listItensProducts;
+    };
+
+    addItensProducts( formData: IItemProductCreate ){
+
+        const newItenProduct = { ...formData, id: crypto.randomUUID() };
+        this.listItensProducts.push( newItenProduct );
+
+    };
     
 }
